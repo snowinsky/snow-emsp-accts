ed7905f992eb60f4b45c909e5c2d874b0a5cf761
snowinsky <snowinsky@sina.cn>
2025-06-06 13:16:49 +0800

    add aws database disable the security in dev environment add some length limit for input values

--- src/main/java/cn/snow/emsp/accts/api/vo/CreateAccountRequest.java
+++ src/main/java/cn/snow/emsp/accts/api/vo/CreateAccountRequest.java
@@ -4,11 +4,13 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 public class CreateAccountRequest implements java.io.Serializable{
 
     @NotNull(message = "email is empty")
+    @Size(min = 5, max = 100, message = "email length must be between 5 and 100")
     @Pattern(regexp = "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$", message = "email format error")
     private String email;
 
--- src/main/java/cn/snow/emsp/accts/api/vo/CreateCardRequest.java
+++ src/main/java/cn/snow/emsp/accts/api/vo/CreateCardRequest.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.Size;
 @Data
 public class CreateCardRequest  implements java.io.Serializable{
     @NotNull(message = "rfidUid is empty")
+    @Size(min = 5, max = 100, message = "rfidUid length must be 5-100")
     private String rfidUid;
     @NotNull(message = "rfidVisibleNumber is empty")
     @Size(min = 16, max = 18, message = "rfidVisibleNumber length must be 16-18")
